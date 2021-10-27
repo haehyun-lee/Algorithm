ece9953be760f6875bcab5e6d953e78719889c87
bada <bada7144@gmail.com>
2021-10-27 23:29:20 +0900

    Add test

---
+++ Programmers/level1/Test.java
@@ -0,0 +1,26 @@
+package level1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str1 = null;
+		String str2 = "";
+		String str3 = " ";
+		
+		System.out.println(isNullOrEmpty(str1));		//true
+		System.out.println(isNullOrEmpty(str2));		//true
+		System.out.println(isNullOrEmpty(str3));		//false
+		
+		System.out.println(isNullOrWhiteSpace(str1));	//true
+		System.out.println(isNullOrWhiteSpace(str2));	//true
+		System.out.println(isNullOrWhiteSpace(str3));	//true
+	}
+	
+	static boolean isNullOrEmpty(String str) {
+		return str == null || str.isEmpty();
+	}
+	
+	static boolean isNullOrWhiteSpace(String str) {
+		return str == null || str.isEmpty() || str.contains(" ");
+	}
+}
