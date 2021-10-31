272aa2a7d55612b6e832c0e6c86d0951c092b8dc
bada <bada7144@gmail.com>
2021-10-31 16:09:30 +0900

    Modify 다트게임

--- Programmers/level1/DartsGame.java
+++ Programmers/level1/DartsGame.java
@@ -3,19 +3,19 @@ package level1;
 //다트 게임
 public class DartsGame {
 	public static int solution(String dartResult) {
-        int sum = 0;
-        int[] scores = new int[3];
+        int totalScore = 0;
+        int[] scores = new int[3];              //3번의 기회 각 점수를 저장
         int turn = 0;
         
         for(int i = 0; i < dartResult.length(); i++){
             char ch = dartResult.charAt(i);
             if(Character.isDigit(ch)){
-                if(scores[turn] != 0) { 
+                if(scores[turn] != 0) {         //해당 기회에 점수가 두번 저장되는 경우는 10뿐.
                     scores[turn] = 10; 
                 }else{
                     scores[turn] = ch - '0';
                 }
-            }else if(Character.isAlphabetic(ch)){
+            }else{
                 switch(ch){
                     case 'D':
                         scores[turn] = (int)(Math.pow(scores[turn], 2));
@@ -23,32 +23,27 @@ public class DartsGame {
                     case 'T':
                         scores[turn] = (int)(Math.pow(scores[turn], 3));
                         break;
-                }
-                if(i == dartResult.length() -1){
-                    break;
-                } else if(Character.isDigit(dartResult.charAt(i+1))){
-                    turn++;
-                }
-            }else{
-                switch(ch){
                     case '*':
                         scores[turn] *= 2;
-                        if(turn != 0){ scores[turn-1] *= 2; }
+                        if(turn != 0) { scores[turn-1] *= 2; }  //1번째 기회에서는 현재 점수만 2배
                         break;
                     case '#':
-                        scores[turn] *= -1;
+                         scores[turn] *= -1;
                         break;
                 }
-                turn++;
+                if(i == dartResult.length() -1){                //마지막 문자일 경우 .charAt(i+1) 하면 index넘어감
+                    break;
+                } else if(Character.isDigit(dartResult.charAt(i+1))){   //다음 문자가 숫자면 이번 기회는 끝인 것
+                    turn++;
+                }
             }
         }
         
         for(int score : scores){
-            sum += score;
-            System.out.print(score + ", ");
+            totalScore += score;
         }
         
-        return sum;
+        return totalScore;
     }
 
 	public static void main(String[] args) {
