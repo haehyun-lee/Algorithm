c6eb4fc9d3a5a16c0250a9412fbde432c3ba5c9d
bada <bada7144@gmail.com>
2021-11-08 16:08:01 +0900

    Modify 실패율

--- Programmers/level1/FailureRate.java
+++ Programmers/level1/FailureRate.java
@@ -3,55 +3,34 @@ package level1;
 import java.util.*;
 
 //실패율 - 프렌즈 사천성
-public class FailureRate {
-	//스테이지 정보
-	static class Stage{
-		int num;			//스테이지 번호
-		double failRate;	//실패율
-		
-		public Stage(int num, double failRate) {
-			this.num = num;
-			this.failRate = failRate;
-		}
-	}
-	
+public class FailureRate {	
 	public static int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-
-        int[] userStopCnts = new int[N+2];		//각 stage에 머물러있는 플레이어 수, 마지막 스테이지를 클릭어한 경우를 포함시키기 위해 N+2
+        HashMap<Integer, Double> map = new HashMap<>();
+        
+        int[] userFailCnts = new int[N+2];		//각 stage에 머물러있는 플레이어 수 
         int[] userTotalCnts = new int[N+1];		//각 stage에 도달한 플레이어 수
-        
-        //스테이지별 머물러 있는 플레이어 수 카운트
-        for(int i=0; i < stages.length; i++) {
-        	userStopCnts[stages[i]]++;    	
+       
+        for(int stage : stages) {
+        	userFailCnts[stage]++;
         }
         
-        //스테이지별 도달 플레이어 수 카운트
-        userTotalCnts[N] = userStopCnts[N] + userStopCnts[N+1];
-        for(int i = N-1; i >= 1; i--) {
-        	userTotalCnts[i] = userStopCnts[i] + userTotalCnts[i+1];	//현재 스테이지 머물러 있는 수 + 다음 스테이지에 도달한 수
+        userTotalCnts[N] = userFailCnts[N]+userFailCnts[N+1]; 
+        for(int i = N-1; i >=1 ; i--) {
+        	userTotalCnts[i] = userFailCnts[i] + userTotalCnts[i+1];
         }
         
-        ArrayList<Stage> failRates = new ArrayList<>();
-        for(int i=1; i <= N; i++) {
-        	if(userTotalCnts[i] == 0) {
-        		failRates.add(new Stage(i, 0));
-        		continue;
+        for(int i = 1; i < userTotalCnts.length; i++) {
+        	if(userFailCnts[i] == 0 || userTotalCnts[i] == 0) {
+        		map.put(i, 0.0);
+        	}else {
+            	map.put(i, (double)userFailCnts[i] / userTotalCnts[i]);
         	}
-
-            //실패율 계산
-        	double failRate = (double)userStopCnts[i] / userTotalCnts[i];
-        	failRates.add(new Stage(i, failRate));
         }
         
-        //Stage 객체의 failRate를 기준으로 내림차순 정렬
-        Collections.sort(failRates, ((o1, o2) -> Double.compare(o2.failRate, o1.failRate)));
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
         
-        for(int i=0; i < failRates.size(); i++) {
-        	answer[i] = failRates.get(i).num;
-        }
-        
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 	
 	public static void main(String[] args) {
