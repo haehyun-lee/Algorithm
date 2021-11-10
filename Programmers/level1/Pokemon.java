05455050444069ebd010e8da9b3ef0a347487b91
bada <bada7144@gmail.com>
2021-11-11 00:30:31 +0900

    Mod 폰켓몬

--- Programmers/level1/Pokemon.java
+++ Programmers/level1/Pokemon.java
@@ -1,11 +1,16 @@
 package level1;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 //포켓몬
 public class Pokemon {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	 public int solution(int[] nums) {        
+        Set<Integer> set = new TreeSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        
+        return Math.min(set.size(), nums.length / 2);
+    }
 }
