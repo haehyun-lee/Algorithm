35f1acd799fffc92b7d124a39b70f0b217a8041a
bada <bada7144@gmail.com>
2021-11-20 20:20:35 +0900

    Add BOJ 기초수학

---
+++ BaekJoon/Ex_10430.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//기초수학
+public class Ex_10430 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		//분배 법칙 : (a + b) x c = a x c + b x c 
+		System.out.println((a + b) % c);
+		System.out.println(((a % c) + (b % c)) % c);
+		System.out.println((a * b) % c);
+		System.out.println(((a % c) * (b % c)) % c);
+	}
+
+}
