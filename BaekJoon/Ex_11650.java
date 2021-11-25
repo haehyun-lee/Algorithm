914c8a9f4d83a63507356ad988cba5c095b42aa3
bada <bada7144@gmail.com>
2021-11-25 23:25:15 +0900

    Add BOJ Sort

---
+++ BaekJoon/Ex_11650.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//좌표 정렬하기
+public class Ex_11650 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] pos = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			 pos[i][0] = sc.nextInt();	//x
+			 pos[i][1] = sc.nextInt();	//y
+			 
+		}
+
+	}
+
+}
