bdd3d6fa240eed4828397e538a1cc0811ed2bc3a
bada <bada7144@gmail.com>
2021-12-27 22:42:26 +0900

    Add ueue

--- Programmers/level2/BridgeTruck.java
+++ Programmers/level2/BridgeTruck.java
@@ -11,36 +11,36 @@ public class BridgeTruck {
 
         Queue<Integer> queue = new LinkedList<>();     // 다리
 
-for (int truck_weight : truck_weights) {
-    // 해당 트럭이 들어가려면 앞 트럭들이 빠질때까지 기다려야하기 때문에, 현재 트럭이 들어갈 때까지 무한 반복
-    while (true) {
-        if (queue.isEmpty()) {                        // 다리가 비어 있을 경우
-            queue.add(truck_weight);
-            sum += truck_weight;
-            time++;
-            break;
-        } else if (queue.size() == bridge_length) {   // 다리가 다 차 있을 경우
-            int truck = queue.poll();
-            sum -= truck;
-            time++;
-            if (sum + truck_weight <= weight) {       // 현재 트럭이 들어가도 무게제한 안 걸릴 경우
-                queue.add(truck_weight);
-                sum += truck_weight;
-                break;
-            } else {
-                queue.add(0);
+        for (int truck_weight : truck_weights) {
+            // 해당 트럭이 들어가려면 앞 트럭들이 빠질때까지 기다려야하기 때문에, 현재 트럭이 들어갈 때까지 무한 반복
+            while (true) {
+                if (queue.isEmpty()) {                        // 다리가 비어 있을 경우
+                    queue.add(truck_weight);
+                    sum += truck_weight;
+                    time++;
+                    break;
+                } else if (queue.size() == bridge_length) {   // 다리가 다 차 있을 경우
+                    int truck = queue.poll();
+                    sum -= truck;
+                    time++;
+                    if (sum + truck_weight <= weight) {       // 현재 트럭이 들어가도 무게제한 안 걸릴 경우
+                        queue.add(truck_weight);
+                        sum += truck_weight;
+                        break;
+                    } else {
+                        queue.add(0);
+                    }
+                } else {                                      // 다리가 다 차지는 않았을 경우
+                    time++;
+                    if (sum + truck_weight <= weight) {
+                        queue.add(truck_weight);
+                        sum += truck_weight;
+                        break;
+                    } else {
+                        queue.add(0);
+                    }
+                }
             }
-        } else {                                      // 다리가 다 차지는 않았을 경우
-            time++;
-            if (sum + truck_weight <= weight) {
-                queue.add(truck_weight);
-                sum += truck_weight;
-                break;
-            } else {
-                queue.add(0);
-            }
-        }
-    }
         }
 
         return time + bridge_length;        // 마지막으로 들어간 트럭이 다리를 지날 시간 추가
