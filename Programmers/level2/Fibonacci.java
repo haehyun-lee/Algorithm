00941491a59068a5bd15b56e8ec99db661a9a66b
bada <bada7144@gmail.com>
2022-01-13 21:01:49 +0900

    Add level2

---
+++ Programmers/level2/Fibonacci.java
@@ -0,0 +1,20 @@
+package level2;
+
+public class Fibonacci {
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+
+        return dp[n] % 1234567;
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibo = new Fibonacci();
+        System.out.println(fibo.solution(3));
+    }
+}
